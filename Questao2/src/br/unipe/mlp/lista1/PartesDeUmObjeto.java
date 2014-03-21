0c3ad19814de237e8a6de577324af7f50aca7f6e
Flaviano César <cesar.flaviano@yahoo.es>
2014-03-21 01:43:29 -0300

    Concluida

---
+++ Questao2/src/br/unipe/mlp/lista1/PartesDeUmObjeto.java
@@ -0,0 +1,78 @@
+package br.unipe.mlp.lista1;
+
+public class PartesDeUmObjeto {
+	
+/*
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  # METODOS: Metodos são blocos de codigo que pertencem a uma classe e tem por finalidade realizar tarefas.
+  
+  Exemplo:
+  
+  public int soma(int num1, int num2){
+  	
+  	return num1+num2;
+  }
+ 
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////// 	
+  	
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////  	
+ # ATRIBUTOS: Atributo é o conjunto de propriedades(variáveis) da classe.
+ 
+ Exemplo:
+ 
+ public class Contas{
+ 
+ int agencia;  // --> é um atributo(variável).
+ double saldo; // --> é um atributo(variável).
+ 
+ }
+ 
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+ # INTERFACE: Interface é um recusrso da orientação a objeto utilizado em java que define 
+   açoes que devem ser obrigatoriamente executadas, mas que cada classe pode executar de 
+   forma diferente. A interface contém valores constantes ou assinaturas de métodos que devem 
+   ser implementados dentro de uma classe.
+   
+   Exemplo:
+   
+   public class TV {
+    
+	private int tamanho;
+	private int canal;
+	private int volume;
+	private boolean ligada;
+	
+   public TV(int tamanho) {
+	this.tamanho = tamanho;
+	this.canal = 0;
+	this.volume = 0;
+	this.ligada = false;
+   }
+}
+
+public interface ControleRemoto {
+
+	void mudarCanal(int canal);  --> Assinatura de metodos implementada.
+	void aumentarVolume (int taxa);  --> Assinatura de metodos implementada.
+	void diminuirVolume (int taxa);  --> Assinatura de metodos implementada.
+	boolean ligar();  --> Assinatura de metodos implementada.
+	boolean desligar();  --> Assinatura de metodos implementada.
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+#IDENTIDADE: É apropriedade de um objeto que o destingue de outros objetos. Identidade não é o nome do objeto,
+nem o endereço de memória onde ele esta armazenado, é um conceito que não é visível para os usuários.
+
+Exemplo: Posso exemplificar através da criação de duas contas em um mesmo Banco, muito embora eles sejam da 
+mesma classe, que os atributos dos mesmos sejam identicos, ainda teriam identidades distintas; seriam objetos 
+diferentes.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ */
+
+}
