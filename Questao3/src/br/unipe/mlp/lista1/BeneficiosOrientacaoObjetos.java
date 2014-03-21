070b9a511e7015fa65baa9ade6fa61d29bd1ff58
Flaviano César <cesar.flaviano@yahoo.es>
2014-03-21 01:44:33 -0300

    Concluida!

---
+++ Questao3/src/br/unipe/mlp/lista1/BeneficiosOrientacaoObjetos.java
@@ -0,0 +1,32 @@
+package br.unipe.mlp.lista1;
+
+public class BeneficiosOrientacaoObjetos {
+
+/*
+ CONCEITO: Orientação a objeto é um conceito que esta relacionado com a idéia de classificar,
+  organizar e abstrair coisas. O termo orientação a objetos significa organizar o mundo real 
+  como uma coleção de objetos que incorporam estrutura de dados e um conjunto de operações 
+  que manipulam estes dados.
+ 
+ VANTAGENS DIRETAS: 
+ 
+ *Maior facilidade para reutilização do código e por consequencia do projeto;
+ *Possibilidade do desenvolvedor trabalhar em um nível mais elevado de abstração;
+ *Utilização de um único padrão conceitual durante todo processo de criação de software;
+ *Maior adequação à arquitetura cliente/servidor;
+ *Maior facilidade de comunicação com os usuários e com outros profissionais do ramo;
+ 
+ VANTAGENS REAIS:
+ 
+ *Ciclo de vida mais longo para os sistemas;
+ *Desenvolvimento acelerado de sistemas;
+ *Possibilidade de se construir sistemas muito mais complexos, pela incorporação de funções prontas;
+ *Menor custo para desenvolvimento e manutenção de sistemas.
+ 
+ 
+ 
+ 
+ 
+ */
+	
+}
