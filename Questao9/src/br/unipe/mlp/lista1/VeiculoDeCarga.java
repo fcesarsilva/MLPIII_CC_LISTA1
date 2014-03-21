eb7fccf567668070e576c16892f3637d5ffb55ab
Flaviano César <cesar.flaviano@yahoo.es>
2014-03-21 01:48:34 -0300

    Concluida!

---
+++ Questao9/src/br/unipe/mlp/lista1/VeiculoDeCarga.java
@@ -0,0 +1,24 @@
+package br.unipe.mlp.lista1;
+
+public class VeiculoDeCarga extends Veiculo{
+
+	private long capacidadeDeCarga;
+
+	/**
+	 * @return the capacidadeDeCarga
+	 */
+	public long getCapacidadeDeCarga() {
+		return capacidadeDeCarga;
+	}
+
+	/**
+	 * @param capacidadeDeCarga the capacidadeDeCarga to set
+	 */
+	public void setCapacidadeDeCarga(long capacidadeDeCarga) {
+		this.capacidadeDeCarga = capacidadeDeCarga;
+	}
+
+	
+	
+	
+}
---
+++ Questao9/src/br/unipe/mlp/lista1/VeiculoDePasseio.java
@@ -0,0 +1,34 @@
+package br.unipe.mlp.lista1;
+
+public class VeiculoDePasseio extends Veiculo{
+	
+	private int qtdPortas;
+	private boolean arCondiciona;
+	/**
+	 * @return the qtdPortas
+	 */
+	public int getQtdPortas() {
+		return qtdPortas;
+	}
+	/**
+	 * @param qtdPortas the qtdPortas to set
+	 */
+	public void setQtdPortas(int qtdPortas) {
+		this.qtdPortas = qtdPortas;
+	}
+	/**
+	 * @return the arCondiciona
+	 */
+	public boolean isArCondiciona() {
+		return arCondiciona;
+	}
+	/**
+	 * @param arCondiciona the arCondiciona to set
+	 */
+	public void setArCondiciona(boolean arCondiciona) {
+		this.arCondiciona = arCondiciona;
+	}
+	
+	
+
+}
