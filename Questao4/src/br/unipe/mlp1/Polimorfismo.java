f599a90c3c7fa907acfc82c8c172ec91113435da
Flaviano César <cesar.flaviano@yahoo.es>
2014-03-21 01:45:12 -0300

    Concluida!

---
+++ Questao4/src/br/unipe/mlp1/Polimorfismo.java
@@ -0,0 +1,31 @@
+package br.unipe.mlp1;
+
+public class Polimorfismo {
+
+/*
+ 
+ POLIFORMISMO: Polimorfismo é a capacidade de um objeto poder ser referenciado de várias formas.
+
+ Exemplo:
+ 
+Imagine que você é o criador do jogo, onde terá vários bichos nesse jogo. Como você é esperto, 
+vai logo abstrair e criar uma classe "Animal" com as características que todos tem: idade, peso, 
+espécie etc. Porém, nesse game, os animais fazem seu som característico: o cachorro late, o 
+gato mia, o pinto pia, a vaca muge etc.
+
+Veja, polimorfismo é isso: embora objetos sejam da mesma superclasse, vão agir de maneira diferente em algum aspecto. Ou seja, terão várias(poli) formas diferentes (morfismo).
+
+A saída é criar um método chamado 'som()' na superclasse (só o cabeçalho, como veremos nos próximos tutoriais) e em cada subclasse criar um método diferente, que caracterize cada bicho.
+
+Veja que se não fizéssemos isso e invocássemos os métodos: vaca.som(), cachorro.som(), gato.som(), todos iriam fazer o mesmo barulho.
+Com o polimorfismo: vaca.som() faria a vaquinha mugir, cachorro.som() faria o cachorro latir e gato.som() faria o objeto miar.
+Porém, todos continuam sendo, também, objetos da classe "Animal".
+
+E quando chegar mais animais na sua fazenda, adicione o método som() nesse animal, de modo que ele poderá agir conforme suas características.
+
+
+Resumindo: polimorfismo permite que uma mesma superclasse possua subclasses com características - ou formas - diferentes.
+ 
+ 
+ */
+}
