d57d48158f85ea497656990313cce3155dc4e050
Flaviano César <cesar.flaviano@yahoo.es>
2014-03-21 01:42:17 -0300

    Concluida!

---
+++ Questao1/src/br/unipe/mlp/lista1/CompilacaoEExecucao.java
@@ -0,0 +1,27 @@
+﻿package br.unipe.mlp.lista1;
+
+public class CompilacaoEExecucao {
+
+/*
+  O Java tem dois processos de execuçaoo de código fonte. A interpretação e a compilação.
+
+- Compilação: 
+
+      Quando criamos uma classe Java e a salvamos com a extensão .java, é nada mais nada menos que código 
+   Java não inteligível pela JVM. Antes de pedir pra que sua máquina execute o código, este mesmo código
+   precisa estar em linguagem de máquina, portanto, compilado.
+   A JVM não consegue entender código Java, mas sim bytecodes, onde os mesmos são resultados de um processo
+   semelhante ao dos compiladores de código-fonte, mas que não é imediatamente executável. Simplificando,
+   é o código gerado quando você compila um arquivo .java, tornando-o .class.
+
+- Interpretação:
+ 
+      O Java também precisa varrer o seu código pra ver se está tudo ok e depois de traduzir esse código, digamos 
+  interpretar-lo, para o entendimento da máquina onde ele está sendo executado. É ai onde entra a JVM, para 
+  interpretar o arquivo.class na linguagem de máquina. E salientamos que a cada execução deste programa a 
+  JVM interpretará o código em todas elas, certificando-se de que tudo está correto e otimizando sempre 
+  que possível, para deixa-lo cada vez mais rápido.
+
+*/
+	
+}
